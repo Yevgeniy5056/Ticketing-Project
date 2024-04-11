361569a55204ba84d6947a2bb53efaf2bfb2051d
Yevgeniy5056 <149609362+Yevgeniy5056@users.noreply.github.com>
2024-04-11 10:28:00 -0400

    UserController fixed create.html fixed roles provided to create page

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.RoleService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
+@AllArgsConstructor
 public class UserController {
+
+    private final RoleService roleService;
 
     @GetMapping("/create")
     public String createUser(Model model) {
 
         model.addAttribute("user", new UserDTO());
-        model.addAttribute("roles",)
+        model.addAttribute("roles", roleService.findAll());
+
         return "/user/create";
     }
 }
