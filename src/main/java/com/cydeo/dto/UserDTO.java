83879773bc98b79ad40ff58b71472827b4ba34be
Yevgeniy5056 <149609362+Yevgeniy5056@users.noreply.github.com>
2024-04-10 22:12:11 -0400

    dto package created with RoleDTO and UserDTO

---
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package com.cydeo.dto;
+
+import com.cydeo.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private boolean isEnable;
+    private RoleDTO role;
+    private Gender gender;
+}
