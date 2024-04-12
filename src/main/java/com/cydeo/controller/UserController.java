3eb479c3dfe12090da9f41976eb95ff1107f2179
Yevgeniy5056 <149609362+Yevgeniy5056@users.noreply.github.com>
2024-04-12 09:56:22 -0400

    update button fixed

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -6,10 +6,7 @@ import com.cydeo.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -33,6 +30,24 @@ public class UserController {
     public String insertUser(@ModelAttribute("user") UserDTO user) {
 
         userService.save(user);
+
+        return "redirect:/user/create";
+    }
+
+    @GetMapping("/update/{userName}")
+    public String updateUser(@PathVariable String userName, Model model) {
+
+        model.addAttribute("user", userService.findById(userName));
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
+
+        return "/user/update";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(@ModelAttribute("user") UserDTO user) {
+
+        userService.update(user);
 
         return "redirect:/user/create";
     }
