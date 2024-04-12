1b9ff0f3e18ca2ac3a1e0a746224a9afdbe9369e
Yevgeniy5056 <149609362+Yevgeniy5056@users.noreply.github.com>
2024-04-12 12:48:25 -0400

    project create page works

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,15 +1,27 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@AllArgsConstructor
 @RequestMapping("/project")
 public class ProjectController {
 
+    private final ProjectService projectService;
+    private final UserService userService;
+
     @GetMapping("/create")
-    public String createProject() {
+    public String createProject(Model model) {
+
+        model.addAttribute("project", new ProjectDTO());
+        model.addAttribute("managers", userService.findAll());
+        model.addAttribute("projects", projectService.findAll());
 
         return "/project/create";
     }
