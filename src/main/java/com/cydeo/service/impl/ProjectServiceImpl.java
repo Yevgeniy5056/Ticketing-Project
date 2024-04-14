23a81f0e97422c73cf2da0e9e183d7b6772dfb5f
Yevgeniy5056 <149609362+Yevgeniy5056@users.noreply.github.com>
2024-04-14 13:19:00 -0400

    project-status page completed

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -1,16 +1,22 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.TaskDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class ProjectServiceImpl extends AbstractMapService<ProjectDTO, String> implements ProjectService {
+
+    private final TaskService taskService;
 
     @Override
     public ProjectDTO save(ProjectDTO project) {
@@ -54,8 +60,18 @@ public class ProjectServiceImpl extends AbstractMapService<ProjectDTO, String> i
                 .filter(project -> project.getAssignManager().equals(manager))
                 .map(project -> {
 
-                    project.setCompleteTaskCounts(5);
-                    project.setUnfinishedTaskCounts(3);
+                    List<TaskDTO> taskList = taskService.findTasksByManager(manager);
+
+                    int completedTaskCounts = (int) taskList.stream()
+                            .filter(task -> task.getProject().equals(project) && task.getTaskStatus() == Status.COMPLETE)
+                            .count();
+
+                    int unfinishedTaskCounts = (int) taskList.stream()
+                            .filter(task -> task.getProject().equals(project) && task.getTaskStatus() != Status.COMPLETE)
+                            .count();
+
+                    project.setCompleteTaskCounts(completedTaskCounts);
+                    project.setUnfinishedTaskCounts(unfinishedTaskCounts);
 
                     return project;
                 })
