447a8e0b19c6b6054272853878c375816484c108
Yevgeniy5056 <149609362+Yevgeniy5056@users.noreply.github.com>
2024-04-14 10:42:27 -0400

    TaskServiceImpl updated, save button works

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,9 +1,11 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.enums.Status;
 import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -11,6 +13,13 @@ public class TaskServiceImpl extends AbstractMapService<TaskDTO, Long> implement
 
     @Override
     public TaskDTO save(TaskDTO task) {
+
+        if (task.getAssignedDate() == null)
+            task.setAssignedDate(LocalDate.now());
+
+        if (task.getTaskStatus() == null)
+            task.setTaskStatus(Status.OPEN);
+
         return super.save(task.getId(), task);
     }
 
